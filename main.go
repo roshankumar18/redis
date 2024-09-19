22d63601c5bdec522c2971d92edb2b76fdd342e0
roshan-helium <houseno14a@gmail.com>
2024-09-20 00:00:48 +0530

    create tcp server

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "net"
+import (
+	"log/slog"
+	"net"
+)
+
+const defaultAddr = ":5666"
 
 type Config struct {
 	ListenerAddreess string
@@ -11,8 +16,36 @@ type Server struct {
 }
 
 func NewSever(config Config) *Server {
+	if len(config.ListenerAddreess) == 0 {
+		config.ListenerAddreess = defaultAddr
+	}
 	return &Server{
 		Config: config,
 	}
 }
-func main() {}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenerAddreess)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+	s.AcceptLoop()
+	return nil
+}
+
+func (s *Server) AcceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("err", err)
+			continue
+		}
+	}
+}
+func main() {
+	server := NewSever(Config{
+		ListenerAddreess: ":8080",
+	})
+	server.Start()
+}
